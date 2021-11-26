0e6761404c55d0bcc7e23f00ab89ed4f5bb962f6
extro <extro@extro.ovh>
2021-11-26 11:01:24 +0100

    un peu de rangement

--- inc/joueur.h
+++ inc/joueur.h
@@ -1,35 +1,36 @@
 /*
- * joueur.h
+ * initialisation_joueur.h
  *
- *  Created on: 19 nov. 2020
+ *  Created on: 18 nov. 2020
  *      Author: lause
  */
 
 #ifndef JOUEUR_H_
 #define JOUEUR_H_
 
-/* idee de laulau mais trop complique :''(
-typedef enum Id Id;
-enum Id Id{
-	BANQUE = 0,
-	JOUEUR1,JOUEUR2,JOUEUR3,JOUEUR4
-};
+#include "carte.h"
 
-*/
+typedef enum{
+	ROUGE,
+	VERT,
+	BLEU,
+	JAUNE,
+	NOIR,
+	BLANC
+}couleur_perso;
 
-class Joueur{
-	//Attributs
-private:
-	int Id;
-	Carte Cartes;
-	int Cagnotte;
+typedef struct{
+	int id;
+	char nom[30];
+	int score;
+	couleur_perso couleur;
+	//int position;
+	//Carte Carte;
+}Joueur;
 
-	//Methodes
-public:
-	void Piocher(Tas,Cartes);
-	int calculScoreCarte(Cartes);
-	int getCagnotte();
-};
+//struct Joueur init(void);
+//void joueurInit(void);
 
+void initJoueur(Joueur joueur[]);
 
 #endif /* JOUEUR_H_ */
